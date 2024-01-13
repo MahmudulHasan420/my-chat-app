e469f8e4417c664e683a79d05ec3303ba8ef2fd7
MahmudulHasan420 <mahmudulhasan13898@gmail.com>
2024-01-13 19:07:07 +0600

    olpo baki

--- src/components/GroupCom.jsx
+++ src/components/GroupCom.jsx
@@ -4,13 +4,27 @@ import profile from "../assets/profile.png"
 
 const GroupCom = () => {
 
-  let [Creatgroup , setCreatGroup] = useState(false)
+  let [creatgroup , setCreatGroup] = useState(false)
+  let [creatName , setCreatName] = useState(false)
+
+  let handlegrouppage =()=>{
+    setCreatGroup(!creatgroup)
+  }
+  let handlegroupName=(e)=>{
+    setCreatName(e.target.value)
+  }
   
   return (
     <div className='chatcom'>
     <div className='chatcomhaeder'>
         <h2>Group</h2>
-        <button className='chatcombtn'  >Creat group</button>
+        {creatgroup ? 
+         
+         <button className='chatcombtn' onClick={handlegrouppage} >Back</button>
+        :
+        <button className='chatcombtn' onClick={handlegrouppage} >Creat group</button>
+        }
+       
     </div>
     <div className='chatcomSearchBox'>
     <input type="text" className='chatcomSearch' />
@@ -20,8 +34,12 @@ const GroupCom = () => {
         <p>Search</p>
     </div>
     </div>
-    {Creatgroup ?
-    <h1>mahmud</h1>
+    {creatgroup ?
+    <div className='flex crategroupinput'>
+    <lebel>group name :</lebel>
+    <input onChange={handlegroupName} type="text" className='chatcomSearch' />
+    <Button   variant="contained" >Confirm</Button>
+    </div>
       :
       <div className='chatcomMsg'>
       <img src={profile} alt="" />
@@ -31,14 +49,7 @@ const GroupCom = () => {
         <p className='chatcomTime'>10:30 PM</p>
       </div>
     </div>}
-    <div className='chatcomMsg'>
-      <img src={profile} alt="" />
-      
-      <div className='chatcomName'>
-      <h5 className='pName'>Jenny Wilson</h5>
-        <p className='chatcomTime'>10:30 PM</p>
-      </div>
-    </div>
+
 
 
     
